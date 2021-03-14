6e526b932ebc6e2f7133f499cd024ba4f1dab148
faberthet <78429398+faberthet@users.noreply.github.com>
2021-03-14 10:38:57 +0100

    Update Grille.java

--- src/modele/Grille.java
+++ src/modele/Grille.java
@@ -5,8 +5,7 @@ public class Grille<T>
 	protected Object[][] cellules ;
 
 	
-	public Grille(int hauteur, int largeur)
-	{
+	public Grille(int hauteur, int largeur){
 		assert hauteur >= 0;
 		assert largeur >= 0;
 		this.hauteur = hauteur ;
@@ -18,33 +17,27 @@ public class Grille<T>
 
 	public int getLargeur() { return largeur ; }
 
-	public boolean coordCorrectes(int lig, int col)
-	{
+	public boolean coordCorrectes(int lig, int col){
 		return 1<=lig && lig <=getHauteur()
 				&& 1<=col && col <=getLargeur() ;
 	}
 
 	@SuppressWarnings("unchecked")
-	public T getCellule(int lig, int col)
-	{
+	public T getCellule(int lig, int col){
 		assert coordCorrectes(lig, col) ;
 		return (T) cellules[lig-1][col-1] ;
 	}
 
-	public void setCellule(int lig, int col, T ch)
-	{
+	public void setCellule(int lig, int col, T ch){
 		assert coordCorrectes(lig, col) ;
 		cellules[lig-1][col-1] = ch ;
 	}
 
 	@Override
-	public String toString()
-	{
+	public String toString(){
 		StringBuffer resultat = new StringBuffer() ;
-		for (int l=1; l<=this.getHauteur(); l++)
-		{
-			for (int c=1; c<=this.getLargeur(); c++)
-			{
+		for (int l=1; l<=this.getHauteur(); l++){
+			for (int c=1; c<=this.getLargeur(); c++){
 				if(c>1) { resultat.append('|') ; }
 				resultat.append(this.getCellule(l, c)) ;
 			}
@@ -53,14 +46,11 @@ public class Grille<T>
 		return resultat.toString() ;
 	}
 
-	public static void main (String[] args)
-	{
+	public static void main (String[] args){
 		Grille<String> maGrille = new Grille<String>(3,5) ;
-		for (int l=1; l<=maGrille.getHauteur(); l++)
-		{
+		for (int l=1; l<=maGrille.getHauteur(); l++){
 			String texteLigne = Integer.toString(l);
-			for (int c=1; c<=maGrille.getLargeur(); c++)
-			{
+			for (int c=1; c<=maGrille.getLargeur(); c++){
 				maGrille.setCellule(l, c, texteLigne + ',' + Integer.toString(c));
 			}
 		}
