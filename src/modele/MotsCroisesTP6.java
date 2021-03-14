924f45da92b042b50d1641e16e5b926671b598ef
faberthet <78429398+faberthet@users.noreply.github.com>
2021-03-14 10:01:39 +0100

    Update MotsCroisesTP6.java

--- src/modele/MotsCroisesTP6.java
+++ src/modele/MotsCroisesTP6.java
@@ -22,7 +22,6 @@ public class MotsCroisesTP6 implements SpecifMotsCroises{
 		}
 	}
 
-	
 	/**
 	 * @return true si le deplacement a partir de la case de coordonées(lig,col)
 	 *  est possible selon le sens
@@ -43,64 +42,53 @@ public class MotsCroisesTP6 implements SpecifMotsCroises{
 		return false;	
 	}
 	
-	public int getHauteur()
-	{
+	public int getHauteur(){
 		return solution.getHauteur() ;
 	}
 
-	public int getLargeur()
-	{
+	public int getLargeur(){
 		return solution.getLargeur() ;
 	}
 
-	public boolean coordCorrectes(int lig, int col)
-	{
+	public boolean coordCorrectes(int lig, int col){
 		return 1<=lig && lig<=getHauteur()
 				 && 1<=col && col<=getLargeur() ;
 	}
 
-	public boolean estCaseNoire(int lig, int col)
-	{
+	public boolean estCaseNoire(int lig, int col){
 		assert coordCorrectes(lig, col) ;
 		return (solution.getCellule(lig, col) == null) ;
 	}
 
-	public void setCaseNoire(int lig, int col, boolean noire)
-	{
+	public void setCaseNoire(int lig, int col, boolean noire){
 		assert coordCorrectes(lig, col) ;
-		if (noire)
-		{
+		if (noire){
 			solution.setCellule(lig, col, null) ;
 		}
-		else if (solution.getCellule(lig, col) == null)
-		{
+		else if (solution.getCellule(lig, col) == null){
 			solution.setCellule(lig, col, Character.valueOf(' ')) ;//new Character(' ')
 		}
 	}
 
-	public char getSolution(int lig, int col)
-	{
+	public char getSolution(int lig, int col){
 		assert coordCorrectes(lig, col) ;
 		assert !estCaseNoire(lig, col) ;
 		return solution.getCellule(lig, col) ;
 	}
 
-	public void setSolution(int lig, int col, char sol)
-	{
+	public void setSolution(int lig, int col, char sol){
 		assert coordCorrectes(lig, col) ;
 		assert !estCaseNoire(lig, col) ;
 		solution.setCellule(lig, col, sol) ;
 	}
 
-	public char getProposition(int lig, int col)
-	{
+	public char getProposition(int lig, int col){
 		assert coordCorrectes(lig, col) ;
 		assert !estCaseNoire(lig, col) ;
 		return proposition.getCellule(lig, col).getValue().charAt(0) ;
 	}
 
-	public void setProposition(int lig, int col, char prop)
-	{
+	public void setProposition(int lig, int col, char prop){
 		assert coordCorrectes(lig, col) ;
 		assert !estCaseNoire(lig, col) ;
 		proposition.getCellule(lig, col).set(prop+""); 
@@ -114,41 +102,33 @@ public class MotsCroisesTP6 implements SpecifMotsCroises{
 		setProposition(lig, col, getSolution(lig,col)); 
 	}
 	
-	public String getDefinition(int lig, int col, boolean horiz)
-	{
+	public String getDefinition(int lig, int col, boolean horiz){
 		assert coordCorrectes(lig, col) ;
 		assert !estCaseNoire(lig, col) ;
-		if (horiz)
-		{
+		if (horiz){
 			return horizontal.getCellule(lig, col) ;
 		}
-		else
-		{
+		else{
 			return vertical.getCellule(lig, col) ;
 		}
 	}
 
-	public void setDefinition(int lig, int col, boolean horiz, String def)
-	{
+	public void setDefinition(int lig, int col, boolean horiz, String def){
 		assert coordCorrectes(lig, col) ;
 		assert !estCaseNoire(lig, col) ;
-		if (horiz)
-		{
+		if (horiz){
 			horizontal.setCellule(lig, col, def) ;
 		}
-		else
-		{
+		else{
 			vertical.setCellule(lig, col, def) ;
 		}
 	}
 
 	@Override
-	public String toString()
-	{
+	public String toString(){
 		return "Solution\n" + solution
 					+ "\nProposition\n" + proposition
 					+ "\nHorizontal\n" + horizontal
 					+ "\nVertical\n" + vertical ;
 	}
-
 }
